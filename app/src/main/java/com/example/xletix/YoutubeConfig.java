85b24b4d7d6240b85ce07195d5740d7698eaadab
vtewes <verena.tewes@gmx.net>
2020-01-11 22:14:07 +0100

    Music button link is now configurable

--- app/src/main/java/com/example/xletix/YoutubeConfig.java
+++ app/src/main/java/com/example/xletix/YoutubeConfig.java
@@ -3,5 +3,7 @@ package com.example.xletix;
 public class YoutubeConfig {
 
     public static String PACKAGE_NAME_YOUTUBE_MUSIC = "com.google.android.apps.youtube.music";
-    public static String URL_YOUTUBE_WORKOUT_MUSIC = "https://music.youtube.com/watch?v=Mnl2T-SNysc";
+    public static String URL_YOUTUBE_WORKOUT_MUSIC = "https://www.youtube.com/watch?v=ly6j02YR8rA";
+    public static String PREFERENCE_ID = "PREFERENCE_ID_YOUTUBE_LINK";
 }
+
